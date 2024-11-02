e8f60a92e51905d13d57aa92f461308d22030630
8rxn <rajaryanvishwakarma10@gmail.com>
2024-11-03 03:52:58 +0530

    working state
    Signed-off-by: 8rxn <rajaryanvishwakarma10@gmail.com>

--- main.go
+++ main.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"context"
+	"encoding/json"
 	"errors"
 	"fmt"
 	"io"
@@ -14,6 +15,13 @@ import (
 )
 
 const keyServerAddr = "serverAddr"
+
+type successResponse struct {
+	Success bool   `json:"success"`
+	Url     string `json:"url"`
+	Slug    string `json:"slug"`
+	Expiry  string `json:"expiry,omitempty"`
+}
 
 func getRedirect(w http.ResponseWriter, r *http.Request) {
 	// ctx := r.Context()
@@ -31,31 +39,57 @@ func getRedirect(w http.ResponseWriter, r *http.Request) {
 	url := routes.GetURL(pas_path)
 	http.Redirect(w, r, url, http.StatusMovedPermanently)
 }
+
 func setUrl(w http.ResponseWriter, r *http.Request) {
 	// ctx := r.Context()
+	var body_json map[string]string
+	body, er := io.ReadAll(r.Body)
 
-	url := r.URL.Query().Get("url")
-	slug := r.URL.Query().Get("slug")
-	expiry := r.URL.Query().Get("expiry")
-
-	if expiry == "" {
-		expiry = "0"
-	}
-
-	expiry_time, err := strconv.Atoi(expiry)
-
-	if err != nil {
-		w.WriteHeader(http.StatusBadRequest)
+	if er != nil {
+		http.Error(w, "Error reading body", http.StatusBadRequest)
 		return
 	}
 
-	if url == "" || slug == "" {
-		w.WriteHeader(http.StatusBadRequest)
+	if err := json.Unmarshal(body, &body_json); err != nil {
+		http.Error(w, "Error parsing request body", http.StatusBadRequest)
+		return
+	}
+
+	url, ok := body_json["url"]
+	if !ok {
+		http.Error(w, "url not found in request", http.StatusBadRequest)
+		return
+	}
+
+	slug, ok := body_json["slug"]
+	if !ok {
+		http.Error(w, "slug not found in request", http.StatusBadRequest)
+		return
+	}
+	expiry_present := true
+	expiry, ok := body_json["expiry"]
+	if !ok {
+		expiry = "0"
+		expiry_present = false
+	}
+	expiry_time, err := strconv.Atoi(expiry)
+	if err != nil {
+		http.Error(w, "expiry time not valid", http.StatusBadRequest)
 		return
 	}
 
 	set_slug := routes.SetShortenedURL(url, slug, int32(expiry_time))
-	io.WriteString(w, "hello route!\n url: "+url+"\n slug: "+set_slug)
+
+	w.Header().Set("Content-Type", "application/json")
+	w.WriteHeader(http.StatusOK)
+	var response successResponse
+	response.Success = true
+	response.Url = url
+	response.Slug = set_slug
+	if expiry_present {
+		response.Expiry = expiry
+	}
+	json.NewEncoder(w).Encode(response)
 }
 
 func main() {
